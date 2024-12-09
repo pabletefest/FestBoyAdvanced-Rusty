9928bce31604f6e1028b734e7b59df4f1f026aae
PabloSF <62628314+pabletefest@users.noreply.github.com>
2024-12-09 17:47:07 +0100

    working on exceptions handling and base functions of the cpu core

--- src/arm7tdmi.rs
+++ src/arm7tdmi.rs
@@ -4,11 +4,15 @@ const PC: u8 = 15;
 
 const MODE_BITS_MASK: u32 = 0x0000001F;
 
+const EXCEPTIONS_HANDLERS_ADDRESSES: [u32; 8] = [0x00000000, 0x00000004, 0x00000008, 0x0000000C, 0x00000010, 0x00000014, 0x00000018, 0x0000001C];
+
+#[derive(Clone, Copy)]
 enum CpuStateMode {
     ARM = 0,
     THUMB = 1
 }
 
+#[derive(Clone, Copy)]
 enum OperationModes {
     User = 16,
     FIQ = 17,
@@ -27,6 +31,17 @@ enum CPSRBitsMask {
     I = 0x00000080,
     F = 0x00000040,
     T = 0x00000020
+}
+
+enum ExceptionType {
+    Reset,
+    UndefinedInstruction,
+    SoftwareInterrupt,
+    PrefetchAbort,
+    DataAbort,
+    AddressExceeds,
+    NormalInterrupt,
+    FastInterrupt
 }
 
 pub struct ARM7TDMI {
@@ -68,11 +83,34 @@ impl ARM7TDMI {
         }
     }
 
+    fn reset(&mut self) {
+
+    }
+
+    fn run_instruction(&mut self) -> u8 {
+        0
+    }
+
     fn set_cpsr_bit(&mut self, bit_mask: CPSRBitsMask) {
         self.cpsr |= bit_mask as u32;
     }
 
     fn clear_cpsr_bit(&mut self, bit_mask: CPSRBitsMask) {
         self.cpsr &= !(bit_mask as u32);
+    }
+
+    fn enter_operation_mode(&mut self, mode: OperationModes) {
+        
+        self.cpsr = (self.cpsr & 0x0000001F) | mode as u32;
+        
+        match mode {
+            OperationModes::User => {},
+            OperationModes::FIQ => {},
+            OperationModes::IRQ => {},
+            OperationModes::Supervisor => {},
+            OperationModes::Abort => {},
+            OperationModes::Undefined => {},
+            OperationModes::System => {} 
+        }
     }
 }
