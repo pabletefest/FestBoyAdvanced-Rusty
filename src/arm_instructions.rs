957fc2094a1c829901a25b4c6c99a89749f6f6e6
pabletefest <stemmerpablo@gmail.com>
2024-12-23 23:52:20 +0100

    Added decoding functions for cond and I bits, and DataProc type opcode bits

--- src/arm_instructions.rs
+++ src/arm_instructions.rs
@@ -1,8 +1,23 @@
 use super::arm7tdmi::ARM7TDMI;
 
 #[inline]
-pub fn decode_arm_opcode_bits(instruction: u32) -> u16 {
+pub fn decode_opcode_format_bits(instruction: u32) -> u16 {
     ((instruction >> 16) as u16 & 0x0FF0) | ((instruction >> 4) as u16 & 0xF)
+}
+
+#[inline]
+pub fn decode_cond_bits(instruction: u32) -> u8 {
+    ((instruction >> 28) & 0xF) as u8
+}
+
+#[inline]
+pub fn decode_dataproc_opcode(instruction: u32) -> u8 {
+    ((instruction >> 21) & 0xF) as u8
+}
+
+#[inline]
+pub fn decode_I_bit(instruction: u32) -> u8 {
+    ((instruction >> 25) & 1) as u8
 }
 
 impl ARM7TDMI {
@@ -19,12 +34,12 @@ mod tests {
         let hi_bits: u8 = 0xFF;
         let expectec_bits: u16 = ((hi_bits as u16) << 4) | lo_bits as u16;
 
-        assert_eq!(expectec_bits, decode_arm_opcode_bits(0x0FF000F0));
+        assert_eq!(expectec_bits, decode_opcode_format_bits(0x0FF000F0));
 
         let lo_bits: u8 = 0x8;
         let hi_bits: u8 = 0x88;
         let expectec_bits: u16 = ((hi_bits as u16) << 4) | lo_bits as u16;
 
-        assert_eq!(expectec_bits, decode_arm_opcode_bits(0xF88FFF8F));
+        assert_eq!(expectec_bits, decode_opcode_format_bits(0xF88FFF8F));
     }
 }
