48ae740b4d9786da23e914e860b2da0497359c3d
PabloSF <62628314+pabletefest@users.noreply.github.com>
2025-04-19 21:03:34 +0200

    Adding cycles counting to run frame func

--- src/gba.rs
+++ src/gba.rs
@@ -19,10 +19,11 @@ impl GBA {
     }
 
     pub fn run_frame(&mut self) {
-        let total_cycles: u32 = 0;
+        let mut total_cycles: u32 = 0;
 
         while total_cycles < CYCLES_PER_FRAME {
             let instruction_executed_cycles: u8 = self.cpu.run_instruction(&mut self.sys_mem);
+            total_cycles += instruction_executed_cycles as u32;
         }
     }
 }
